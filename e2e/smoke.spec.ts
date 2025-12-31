92b4d505caa0d41c9e9f373fc1e843bd043ca0e7
Anton <suprun.anton@gmail.com>
2025-12-31 19:52:26 +0100

    HB-3 Create app skeleton

---
+++ e2e/smoke.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('playwright runs', async ({ page }) => {
+  expect(true).toBe(true);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './assets/logo.svg'
+import appname from './assets/appname.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={logo} className="logo" alt="App logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={appname} className="logo" alt="App name" />
+        </a>
+      </div>
+      <div className="card">
+        <h2 className="main-text">
+          Your personal nutrition coaching companion for tracking progress, building custom meal plans, and achieving lasting weight loss results.
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
